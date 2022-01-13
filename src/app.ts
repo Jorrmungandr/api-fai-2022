8758c192c93cc85fd5a4348f977bc0053e45ea00
Edgar Marques <edgarmarques.110@gmail.com>
2022-01-12 22:17:07 -0300

    Main: Add base middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from 'express';
 
+import { bodyParser, contentType, cors } from '@/middlewares';
+
 const app = express();
+
+// Middlewares
+app.use(bodyParser);
+app.use(cors);
+app.use(contentType);
 
 export default app;
